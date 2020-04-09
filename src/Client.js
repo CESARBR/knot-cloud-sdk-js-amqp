9bf58942afc558ee971b7a5afe49fe77b1df0908
Esdras Júnior <eblsj@cesar.org.br>
2020-04-09 09:55:25 -0300

    client: Add things SDK methods
    Add a basic interface to thing control methods, allowing users to
    interact with KNoT Cloud. The operations added are the following:
    
    - register: register a new thing
    - unregister: unregister an existing thing
    - getDevices: list all registered things
    - updateSchema: update the schema of an existing thing
    - publishData: publish thing data on the Storage
    - getData: request for thing to publish data
    - setData: update the current thing data
    - on: listen on events to occur (avaliable: device, schema, data)

---
+++ src/Client.js
@@ -0,0 +1,76 @@
+import AMQP from './network/AMQP';
+
+// Based on websocket interface: https://github.com/CESARBR/knot-cloud-websocket#methods
+export default class Client {
+  constructor(config) {
+    this.headers = { Authorization: config.token };
+    this.amqp = new AMQP(config);
+  }
+
+  async connect() {
+    return this.amqp.start();
+  }
+
+  async close() {
+    return this.amqp.stop();
+  }
+
+  register(id, name) {
+    const msg = { id, name };
+    this.amqp.publishMessage('fogIn', 'device.register', msg, this.headers);
+  }
+
+  unregister(id) {
+    const msg = { id };
+    this.amqp.publishMessage('fogIn', 'device.unregister', msg, this.headers);
+  }
+
+  getDevices() {
+    const msg = {};
+    this.amqp.publishMessage('fogIn', 'device.cmd.list', msg, this.headers);
+  }
+
+  updateSchema(id, schemaList) {
+    const msg = { id, schema: schemaList };
+    this.amqp.publishMessage('fogIn', 'schema.update', msg, this.headers);
+  }
+
+  publishData(id, dataList) {
+    const msg = { id, data: dataList };
+    this.amqp.publishMessage('fogIn', 'data.publish', msg, this.headers);
+  }
+
+  getData(id, sensorIds) {
+    const msg = { id, sensorIds };
+    this.amqp.publishMessage('connOut', 'data.request', msg, this.headers);
+  }
+
+  setData(id, dataList) {
+    const msg = { id, data: dataList };
+    this.amqp.publishMessage('connOut', 'data.update', msg, this.headers);
+  }
+
+  on(event, callback) {
+    let exchange;
+    let routingKeys;
+
+    switch (event) {
+      case 'device':
+        exchange = 'fogOut';
+        routingKeys = ['device.registered', 'device.unregistered', 'device.list'];
+        break;
+      case 'schema':
+        exchange = 'fogOut';
+        routingKeys = ['schema.updated'];
+        break;
+      case 'data':
+        exchange = 'fogIn';
+        routingKeys = ['data.publish'];
+        break;
+      default:
+    }
+
+    const queue = `client-${exchange}-${event}`;
+    this.amqp.subscribeTo(exchange, routingKeys, queue, callback);
+  }
+}
