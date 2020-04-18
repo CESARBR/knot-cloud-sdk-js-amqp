0df279863b4d0f89c85f617f0bbe76865d7ee746
Leonardo Melo <lcbm@cesar.school>
2020-04-17 23:18:02 -0300

    chore: add rollup js module bundler

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import autoExternal from 'rollup-plugin-auto-external';
+import babel from 'rollup-plugin-babel';
+
+import pkg from './package.json';
+
+export default [
+  {
+    input: 'src/index.js',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
+    ],
+    plugins: [
+      autoExternal(),
+      babel({
+        exclude: [
+          'node_modules/**',
+        ],
+      }),
+    ],
+  },
+];
