d7854f81ebcb352ea9c2d87def1c93eadf309987
Esdras Júnior <eblsj@cesar.org.br>
2020-04-27 13:28:34 -0300

    tests: Create Client.test.js test file
    In order to make the SDK code more reliable, this patch add a file to
    test Client features. The first test code operation inspect the
    "register" operation processing.

---
+++ src/Client.test.js
@@ -0,0 +1,102 @@
+import AMQP, * as amqpMocks from './network/AMQP';
+import * as api from './config/api';
+import Client from './Client';
+
+jest.mock('./network/AMQP');
+
+const mockToken = 'my-authorization-token';
+const mockThing = {
+  id: 'abcdef1234568790',
+  name: 'my-device',
+  schema: [
+    {
+      sensorId: 0,
+      typeId: 65521,
+      valueType: 3,
+      unit: 0,
+      name: 'bool-sensor',
+    },
+  ],
+};
+
+const errors = {
+  // Client methods
+  register: 'error registering thing',
+  // AMQP methods
+  start: 'error while starting AMQP connection',
+  stop: 'error while stopping AMQP connection',
+  publishMessage: 'error publishing message on broker',
+  subscribeTo: 'error subscribing consumer on queue',
+  unsubscribeConsumer: 'error unsubscribing consumer',
+};
+
+const registerUseCases = [
+  {
+    testName: 'should register when there is no error at all',
+    amqpOptions: {
+      responseMessage: { id: mockThing.id },
+    },
+  },
+  {
+    testName: 'should fail to register when response has some error',
+    amqpOptions: {
+      responseMessage: { id: mockThing.id, error: errors.register },
+    },
+    expectedErr: errors.register,
+  },
+  {
+    testName: 'should fail to register when unable to publish to channel',
+    amqpOptions: {
+      publishErr: errors.publishMessage,
+    },
+    expectedErr: errors.publishMessage,
+  },
+  {
+    testName: 'should fail to register when unable to subscribe on channel',
+    amqpOptions: {
+      subscribeErr: errors.subscribeTo,
+    },
+    expectedErr: errors.subscribeTo,
+  },
+  {
+    testName: 'should register even when unable to unsubscribe consumer',
+    amqpOptions: {
+      responseMessage: { id: mockThing.id },
+      unsubscribeErr: errors.unsubscribeConsumer,
+    },
+  },
+];
+
+describe('Client', () => {
+  beforeEach(() => {
+    amqpMocks.mockStart.mockClear();
+    amqpMocks.mockStop.mockClear();
+    amqpMocks.mockPublishMessage.mockClear();
+    amqpMocks.mockSubscribeTo.mockClear();
+    amqpMocks.mockUnsubscribeConsumer.mockClear();
+  });
+
+  registerUseCases.forEach((useCase) => {
+    const { testName, amqpOptions, expectedErr } = useCase;
+
+    test(`register: ${testName}`, async () => {
+      const amqp = new AMQP(amqpOptions);
+      const client = new Client(mockToken, amqp, api);
+      let response;
+      let error;
+
+      try {
+        response = await client.register(mockThing.id, mockThing.name);
+      } catch (err) {
+        error = err.message;
+      }
+
+      if (response) {
+        expect(response).toMatchObject(amqpOptions.responseMessage);
+      }
+      if (error) {
+        expect(error).toBe(expectedErr);
+      }
+    });
+  });
+});
