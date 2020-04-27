f037e209e91ce643696a9eb2ddc1e463775499c1
Esdras Júnior <eblsj@cesar.org.br>
2020-04-27 12:35:52 -0300

    network: Create a mock interface for AMQP module

---
+++ src/network/__mocks__/AMQP.js
@@ -0,0 +1,56 @@
+export const mockStart = jest.fn();
+export const mockStop = jest.fn();
+export const mockPublishMessage = jest.fn();
+export const mockSubscribeTo = jest.fn();
+export const mockUnsubscribeConsumer = jest.fn();
+
+export default jest.fn().mockImplementation((options = {}) => {
+  let mockCallback = () => {};
+
+  // start
+  if (options.startErr) {
+    mockStart.mockRejectedValue(Error(options.startErr));
+  } else {
+    mockStart.mockResolvedValue();
+  }
+
+  // stop
+  if (options.stopErr) {
+    mockStop.mockRejectedValue(Error(options.stopErr));
+  } else {
+    mockStop.mockResolvedValue();
+  }
+
+  // publishMessage
+  mockPublishMessage.mockImplementation(() => {
+    if (options.publishErr) {
+      return Promise.reject(Error(options.publishErr));
+    }
+    mockCallback(options.responseMessage);
+    return Promise.resolve(options.publishRet);
+  });
+
+  // subscribeTo
+  mockSubscribeTo.mockImplementation((e, t, k, q, callback) => {
+    if (options.subscribeErr) {
+      return Promise.reject(Error(options.subscribeErr));
+    }
+    mockCallback = callback;
+    return Promise.resolve(options.subscribeRet);
+  });
+
+  // unsubscribeConsumer
+  if (options.unsubscribeErr) {
+    mockUnsubscribeConsumer.mockRejectedValue(Error(options.unsubscribeErr));
+  } else {
+    mockUnsubscribeConsumer.mockResolvedValue();
+  }
+
+  return {
+    start: mockStart,
+    stop: mockStop,
+    publishMessage: mockPublishMessage,
+    subscribeTo: mockSubscribeTo,
+    unsubscribeConsumer: mockUnsubscribeConsumer,
+  };
+});
