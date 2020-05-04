8426d4c8a46be138f79fd3143922b03772699343
João Neto <joao.alexandre@cesar.org.br>
2020-05-04 16:24:14 -0300

    config: Make API compatible with its new version
    Recently, the AMQP API was updated to turn the architecture more
    evolutive and increse scalability. In order to achieve that, this
    patch updates the API defition based on the following reference:
    CESARBR/knot-babeltower/blob/master/docs/events.md.

--- src/config/api.js
+++ src/config/api.js
@@ -2,48 +2,51 @@
 
 export const REGISTER_DEVICE = 'device.register';
 export const UNREGISTER_DEVICE = 'device.unregister';
-export const LIST_DEVICES = 'device.cmd.list';
-export const UPDATE_SCHEMA = 'schema.update';
-export const PUBLISH_DATA = 'data.publish';
-
+export const AUTH_DEVICE = 'device.auth';
+export const LIST_DEVICES = 'device.list';
+export const UPDATE_SCHEMA = 'device.schema.sent';
+export const PUBLISH_DATA = 'data.published';
 export const REGISTER_DEVICE_RESPONSE = 'device.registered';
 export const UNREGISTER_DEVICE_RESPONSE = 'device.unregistered';
-export const LIST_DEVICES_RESPONSE = 'device.list';
-export const UPDATE_SCHEMA_RESPONSE = 'schema.updated';
+export const UPDATE_SCHEMA_RESPONSE = 'device.schema.updated';
 export const REQUEST_DATA = 'data.request';
 export const UPDATE_DATA = 'data.update';
 
-const exchanges = {
-  fogIn: [
-    REGISTER_DEVICE,
-    UNREGISTER_DEVICE,
-    LIST_DEVICES,
-    UPDATE_SCHEMA,
-    PUBLISH_DATA,
-  ],
-  fogOut: [
-    REGISTER_DEVICE_RESPONSE,
-    UNREGISTER_DEVICE_RESPONSE,
-    LIST_DEVICES_RESPONSE,
-    UPDATE_SCHEMA_RESPONSE,
-  ],
-  connOut: [
-    REQUEST_DATA,
-    UPDATE_DATA,
-  ],
+export const DEVICE_EXCHANGE = 'device';
+export const DEVICE_EXCHANGE_TYPE = 'direct';
+
+export const DATA_SENT_EXCHANGE = 'data.sent';
+export const DATA_SENT_EXCHANGE_TYPE = 'fanout';
+
+const requestExchanges = {
+  [DEVICE_EXCHANGE]: {
+    type: DEVICE_EXCHANGE_TYPE,
+    keys: [
+      AUTH_DEVICE,
+      LIST_DEVICES,
+      REGISTER_DEVICE,
+      REGISTER_DEVICE_RESPONSE,
+      UNREGISTER_DEVICE,
+      UNREGISTER_DEVICE_RESPONSE,
+      UPDATE_SCHEMA,
+      UPDATE_SCHEMA_RESPONSE,
+      REQUEST_DATA,
+      UPDATE_DATA,
+    ],
+  },
 };
 
 const responses = {
   [REGISTER_DEVICE]: REGISTER_DEVICE_RESPONSE,
   [UNREGISTER_DEVICE]: UNREGISTER_DEVICE_RESPONSE,
-  [LIST_DEVICES]: LIST_DEVICES_RESPONSE,
   [UPDATE_SCHEMA]: UPDATE_SCHEMA_RESPONSE,
 };
 
 export const getExchange = (key) => {
-  const [exchange] = Object.entries(exchanges)
-    .find(([, keys]) => keys.includes(key)) || [];
-  return exchange;
+  const [name] = Object
+    .entries(requestExchanges)
+    .find(([, obj]) => obj.keys.includes(key)) || [];
+  return name;
 };
 
 export const getResponseKey = (key) => responses[key];
